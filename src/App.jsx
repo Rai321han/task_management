4f66b81a6dc59cbf0bb70973d054696519a61aad
Raihan Uddin <uddinraihan797@gmail.com>
2026-01-19 17:03:32 +0600

    routes moved to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+import Nav from './components/Nav.jsx'
+import TaskListPage from './pages/TaskListPage.jsx'
+import TaskDetailsPage from './pages/TaskDetailsPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import { Routes, Route } from 'react-router'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Routes>
+        <Route element={<Nav />}>
+          <Route index element={<HomePage />} />
+          <Route path='/tasks' element={<TaskListPage />}/>
+          <Route path='/tasks/:id' element={<TaskDetailsPage />}/>
+        </Route>
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Routes>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import './index.css'
-import Nav from './components/Nav.jsx'
-import TaskListPage from './pages/TaskListPage.jsx'
-import TaskDetailsPage from './pages/TaskDetailsPage.jsx'
-import HomePage from './pages/HomePage.jsx'
+import { BrowserRouter  } from 'react-router'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-import NotFoundPage from './pages/NotFoundPage.jsx'
-
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
     <BrowserRouter>
-      <Routes>
-        <Route element={<Nav />}>
-          <Route index element={<HomePage />} />
-          <Route path='/tasks' element={<TaskListPage />}/>
-          <Route path='/tasks/:id' element={<TaskDetailsPage />}/>
-        </Route>
-        <Route path='*' element={<NotFoundPage/>}/>
-      </Routes>
-
+     <App/>
     </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
