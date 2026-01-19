c0159bd399c91594c6382566a2369be1fe69f090
Raihan Uddin <uddinraihan797@gmail.com>
2026-01-19 11:48:49 +0600

    search box placeholder change

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ export default function Search({value, onChange}) {
   return (
     <div>
       <form>
-        <input value={value} onChange={(e) => onChange(e.target.value)} placeholder="Search title in this page" className="px-3 py-2 border-b outline-none border-secondary"/>
+        <input value={value} onChange={(e) => onChange(e.target.value)} placeholder="Search within this page" className="px-3 py-2 border-b outline-none border-secondary"/>
       </form>
     </div>
   );
